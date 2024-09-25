72c2ad66bcd15e22becb2cb07b736184a7534544
Alessio Gogna <alexg@localhost>
2024-09-25 22:07:17 +0200

    Bootstrap Flux

---
+++ scripts/bootstrap_flux.sh
@@ -0,0 +1,10 @@
+flux bootstrap github \
+  --components-extra=image-reflector-controller,image-automation-controller \
+  --token-auth=false \
+  --read-write-key=true \
+  --owner=alecsg77 \
+  --repository=elysium \
+  --branch=main \
+  --path=clusters/kyrion \
+  --private=false \
+  --personal
